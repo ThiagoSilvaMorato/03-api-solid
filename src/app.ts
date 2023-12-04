f575b7964f697f6f532e402f14c92c56db290e36
ThiagoMorato <thiagosmorato@gmail.com>
2023-12-04 12:28:14 -0300

    Finished third module 'Caso de uso e design patterns'

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,22 @@
 import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
+import { appRoutes } from "./http/routes";
+import { ZodError } from "zod";
+import { env } from "./env";
 
 export const app = fastify();
 
-const prisma = new PrismaClient();
+app.register(appRoutes);
 
-prisma.user.create({
-  data: {
-    name: "Thiago Morato",
-    email: "thiago@rocketseat.com.br",
-  },
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({ message: "validation error.", issues: error.format() });
+  }
+
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
+  } else {
+    // TODO: Here we should log to an external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: "Internal server error." });
 });
