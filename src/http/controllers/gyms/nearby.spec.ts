851d511137051ff84436dedcfd0f9f85c74c454e
ThiagoMorato <thiagosmorato@gmail.com>
2024-02-02 14:39:56 -0300

    Finished class 'Testes E2E de rotas de academias' of module 'Controllers & Testes E2E'

---
+++ src/http/controllers/gyms/nearby.spec.ts
@@ -0,0 +1,51 @@
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user";
+
+describe("Nearby Gyms (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to list nearby gyms", async () => {
+    const { token } = await createAndAuthenticateUser(app);
+
+    await request(app.server).post("/gyms").set("Authorization", `Bearer ${token}`).send({
+      title: "JavaScript Gym",
+      description: "Some description.",
+      phone: "11999999999",
+      latitude: -21.2604475,
+      longitude: -48.3290048,
+    });
+
+    await request(app.server).post("/gyms").set("Authorization", `Bearer ${token}`).send({
+      title: "TypeScript Gym",
+      description: "Some description.",
+      phone: "11999999999",
+      latitude: -5.2281626,
+      longitude: -50.7791442,
+    });
+
+    const response = await request(app.server)
+      .get("/gyms/nearby")
+      .query({
+        latitude: -21.2604475,
+        longitude: -48.3290048,
+      })
+      .set("Authorization", `Bearer ${token}`)
+      .send();
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body.gyms).toHaveLength(1);
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        title: "JavaScript Gym",
+      }),
+    ]);
+  });
+});
--- src/http/controllers/gyms/nearby.ts
+++ src/http/controllers/gyms/nearby.ts
@@ -1,14 +1,13 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { z } from "zod";
-import { makeCreateGymUseCase } from "@/use-cases/factories/make-create-gym-use-case";
 import { makeFetchNearbyGymUseCase } from "@/use-cases/factories/make-fetch-nearby-gym-use-case";
 
 export async function nearby(request: FastifyRequest, reply: FastifyReply) {
   const nearbyGymQuerySchema = z.object({
-    latitude: z.number().refine((value) => {
+    latitude: z.coerce.number().refine((value) => {
       return Math.abs(value) <= 90;
     }),
-    longitude: z.number().refine((value) => {
+    longitude: z.coerce.number().refine((value) => {
       return Math.abs(value) <= 180;
     }),
   });
