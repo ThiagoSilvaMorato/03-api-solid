3725ebcd444889d5c27d9e2619f791ba3bce6517
ThiagoMorato <thiagosmorato@gmail.com>
2024-02-02 14:14:28 -0300

    Finished class 'Outros controllers da academia' of module 'Controllers & Testes E2E'

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,27 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { z } from "zod";
+import { makeCreateGymUseCase } from "@/use-cases/factories/make-create-gym-use-case";
+import { makeFetchNearbyGymUseCase } from "@/use-cases/factories/make-fetch-nearby-gym-use-case";
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const nearbyGymQuerySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90;
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180;
+    }),
+  });
+
+  const { latitude, longitude } = nearbyGymQuerySchema.parse(request.body);
+  const fetchNearbyGymsUseCase = makeFetchNearbyGymUseCase();
+
+  const { gyms } = await fetchNearbyGymsUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  });
+
+  return reply.status(200).send({
+    gyms,
+  });
+}
