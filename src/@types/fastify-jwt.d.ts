d1834ee737627b72218a6f3db0ecac2876091990
ThiagoMorato <thiagosmorato@gmail.com>
2024-02-07 14:29:20 -0300

    Finished class 'Autorização por cargos (RBAC)' of module 'Refresh Token & RBAC'

--- src/@types/fastify-jwt.d.ts
+++ src/@types/fastify-jwt.d.ts
@@ -2,11 +2,9 @@ import "@fastify/jwt";
 
 declare module "@fastify/jwt" {
   export interface FastifyJWT {
-    payload: {
-      role: "ADMIN" | "MEMBER";
-    };
     user: {
       sub: string;
+      role: "ADMIN" | "MEMBER";
     }; // user type is return type of 'request.user' object
   }
 }
